31f8ba0a406c43b0bf5356f6dffd0831923bf626
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2023-11-09 20:40:06 -0500

    Adding to my Github repo

---
+++ practice.py
@@ -0,0 +1,29 @@
+# Learning for loops
+
+# nums = [1,2,3,4]
+# res = 0
+
+# for x in nums:
+#     if x % 2 == 0:
+#         continue
+#     else:
+#         res += x
+# print(res)
+
+# Learning for loops part 2 
+
+m = [1,4,2]
+
+for x in m:
+    print(x)
+
+
+
+
+
+
+
+
+
+
+
