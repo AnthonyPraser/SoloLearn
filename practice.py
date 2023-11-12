a8efdeca3ef4ae67d62824715cb38dc72364f221
Anthony P <147562280+AnthonyPraser@users.noreply.github.com>
2023-11-11 19:18:48 -0500

    Adding to my Github repo

--- practice.py
+++ practice.py
@@ -12,10 +12,43 @@
 
 # Learning for loops part 2 
 
-m = [1,4,2]
+# m = [1,4,2]
 
-for x in m:
-    print(x)
+# for x in m:
+#     print(x)
+
+
+# Learning Ranges
+
+# nums = list(range(3, 15, 3))
+# print(nums[2])
+
+# Learning Ranges part 2
+
+# numbers = list(range(5, 10, 2))
+# print(numbers)
+
+# Learning Ranges part 3
+
+# x = list(range(0, 100))
+# print(x[4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
